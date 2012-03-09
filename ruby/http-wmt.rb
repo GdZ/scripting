b9cf9465433dddcadab0fdd92db8bf131ed2bf0f
Herry Wang <herry@herry-desktop.(none)>
2012-03-09 14:06:25 -0800

    disable post when play fail

--- ruby/http-wmt.rb
+++ ruby/http-wmt.rb
@@ -9,60 +9,85 @@ require "rubygems"
 require "curb"
 require "xmlsimple"
 class HttpWMTGet
-    attr_accessor :speed,:url,:duration,:verbose,:logstat,:client_id
-    def initialize(url)
+   attr_accessor :speed,:url,:duration,:verbose,:logstat,:client_id
+   def initialize(url)
 
-        @speed = 1.0  # declare local var to store arg
-        @url = url # declare local var to store arg
-        @duration = 999
-        @verbose = false 
-        @logstat = true
-        @total_bytes  = 0
-        @total_packets = 0
-        @client_id = ''
-        @asxfile = true if /\.asx$/ =~ url
-    end
-    def speed=(s)
-        @speed = s
-    end
-    def duration=(d)
-        @duration=d
-    end
-    def verbose=(v)
-        @verbose = v
-    end
-    def url=(u)
-        @url = u
-    end
+      @speed = 1.0  # declare local var to store arg
+      @url = url # declare local var to store arg
+      @duration = 999
+      @verbose = false 
+      @logstat = true
+      @total_bytes  = 0
+      @total_packets = 0
+      @client_id = ''
+      @asxfile = true if /\.asx$/ =~ url
+      @post_log = true
+   end
+   def speed=(s)
+      @speed = s
+   end
+   def duration=(d)
+      @duration=d
+   end
+   def verbose=(v)
+      @verbose = v
+   end
+   def url=(u)
+      @url = u
+   end
 
-    #
-    #Windows Media Player will disconnect TCP actively when receiving EOS.
-    #For this client, WMS will close TCP right after EOS msg. (Why ?), so client don't have to close connection explicitly
-    #But CDS-IS Streamer Can't disconnect TCP it self after sending out EOS message to client,
-    # streamer will send keep-alive to client after 90s. 30s later, client close connection.
-    #But for Curl.Easy, it's not possible to disconnect TCP to server actively.
-    #So client need wait for 2 mins(default) to be timeout.
-    #One solution is set timeout to less time, like 10s and capture the timeout error
-    #Another solution is use Async HTTP client, like em-http-request
-    # https://github.com/igrigorik/em-http-request
-    #
-    #Update: curb do support Multile interfaces !
-    #So the idea is: using Easy one to send the first request, then add this one into multil so that we can monitor this socket
-    #
-    #Update: There is issue when using Multiple interface:
-    # New connection will be created, instead of using the existing one. set pipeline doesn't help
-    # It's working well with C language. 
-    # It works perfectly on C.
-    #
-    #Update: Curb author's comments. 
-    #https://github.com/taf2/curb/issues/108#issuecomment-4054668
-    #Using 3 easy interfaces, 2 Get + 1 Post
-    #To fix the abort issue, just return -1 in on_body and capture one exception.
+   #
+   #Windows Media Player will disconnect TCP actively when receiving EOS.
+   #For this client, WMS will close TCP right after EOS msg. (Why ?), so client don't have to close connection explicitly
+   #But CDS-IS Streamer Can't disconnect TCP it self after sending out EOS message to client,
+   # streamer will send keep-alive to client after 90s. 30s later, client close connection.
+   #But for Curl.Easy, it's not possible to disconnect TCP to server actively.
+   #So client need wait for 2 mins(default) to be timeout.
+   #One solution is set timeout to less time, like 10s and capture the timeout error
+   #Another solution is use Async HTTP client, like em-http-request
+   # https://github.com/igrigorik/em-http-request
+   #
+   #Update: curb do support Multile interfaces !
+   #So the idea is: using Easy one to send the first request, then add this one into multil so that we can monitor this socket
+   #
+   #Update: There is issue when using Multiple interface:
+   # New connection will be created, instead of using the existing one. set pipeline doesn't help
+   # It's working well with C language. 
+   # It works perfectly on C.
+   #
+   #Update: Curb author's comments. 
+   #https://github.com/taf2/curb/issues/108#issuecomment-4054668
+   #Using 3 easy interfaces, 2 Get + 1 Post
+   #To fix the abort issue, just return -1 in on_body and capture one exception.
 
-    def do_download()
-        puts "URL  == #{@url}"
-        puts "Speed == #{@speed}"
-        c = Curl::Easy.new("#{@url}") do |curl|
+   def do_download()
+      puts "URL  == #{@url}"
+      puts "Speed == #{@speed}"
+      c = Curl::Easy.new("#{@url}") do |curl|
+         curl.headers= ['User-Agent: NSPlayer/11.0.5721.5251']
+         curl.headers+= ['X-Accept-Authentication: Negotiate, NTLM, Digest, Basic']
+         curl.headers+= ['Pragma: version11-enabled=1']
+         curl.headers+= ['Pragma: no-cache,rate=1.000,stream-time=0,stream-offset=0:0,packet-num=4294967295,max-duration=0']
+         curl.headers+= ['Pragma: packet-pair-experiment=1']
+         curl.headers+= ['Pragma: pipeline-experiment=1']
+         curl.headers+= ['Supported: com.microsoft.wm.srvppair, com.microsoft.wm.sswitch, com.microsoft.wm.predstrm, com.microsoft.wm.startupprofilea']
+         curl.headers+= ['Accept-Language: en-US, *;q=0.1']
+         #curl.follow_location = true
+         # curl.verbose = true
+      end
+
+      c.on_body { |data| 
+         print "Get One body with length: "+ data.length.to_s + "\r\n"
+         data.length
+      }
+      #Send first request to get client_id
+      c.perform
+      #@url = c.last_effective_url
+      #puts "last effetive url is " + @url
+      if c.response_code == 302
+         @url = c.header_str.scan(/Location:\s+(.*)\r\n/).flatten[0]
+         c.close
+         c = Curl::Easy.new("#{@url}") do |curl|
             curl.headers= ['User-Agent: NSPlayer/11.0.5721.5251']
             curl.headers+= ['X-Accept-Authentication: Negotiate, NTLM, Digest, Basic']
             curl.headers+= ['Pragma: version11-enabled=1']
@@ -71,80 +96,81 @@ class HttpWMTGet
             curl.headers+= ['Pragma: pipeline-experiment=1']
             curl.headers+= ['Supported: com.microsoft.wm.srvppair, com.microsoft.wm.sswitch, com.microsoft.wm.predstrm, com.microsoft.wm.startupprofilea']
             curl.headers+= ['Accept-Language: en-US, *;q=0.1']
-            curl.follow_location = true
+            #curl.follow_location = true
             # curl.verbose = true
-        end
+         end
+         c.perform
+      end
+      if c.response_code != 200
+         print "HTTP return code is "+ c.response_code.to_s + " exit\r\n"
+         #@post_log = false
+         #return -1
+         exit -1
+      else
+         print "HTTP return code is 200, Good\r\n"
+      end
+      content_len = '0'
+      content_len = c.header_str.scan(/Content-Length:\s+(\d+)/).flatten[0]
+      @client_id = c.header_str.scan(/client-id=(\d+)/).flatten[0]
+      #Get client ID
+      puts "CLIENT ID is #{@client_id} "
+      #puts "Content-Length is #{content_len}"
 
-        c.on_body { |data| 
+      c.headers = ["User-Agent: NSPlayer/11.0.5721.5251"]
+      c.headers += ["Pragma: no-cache,rate=1.000,stream-time=0,stream-offset=4294967295:4294967295,packet-num=4294967295,max-duration=0"]
+      if @speed == 1.0 
+         c.headers += ["Pragma: xPlayStrm=1","Pragma: LinkBW=2147483647\, AccelBW=2147483647\, AccelDuration=10000"]
+      else 
+         c.headers += ["Pragma: xPlayStrm=1","Pragma: LinkBW=2147483647\, AccelBW=2147483647\, AccelDuration=10000, Speed=#{@speed}"]
+      end
+      c.headers += ["Pragms: client-id=#{@client_id}"]
+      c.headers += ["Supported: com.microsoft.wm.srvppair, com.microsoft.wm.sswitch, com.microsoft.wm.predstrm"]
+      c.headers += ["Pragma: xClientGUID={3300AD50-2C39-46c0-AE0A-4FAB6317C555}"]
+      c.headers += ["Pragma: stream-switch-count=2"]
+      c.headers += ["Pragma: stream-switch-entry=ffff:1:0 ffff:2:0"]
+      c.headers += ["Accept-Language: en-us, *;q=0.1"]
+      @total_packets = 0
+      c.on_body { |data| 
+         if @verbose
             print "Get One body with length: "+ data.length.to_s + "\r\n"
+         end
+         print "#"
+         @total_bytes += data.length
+         @total_packets += 1
+         #End of Stream $E
+         if c.total_time > @duration
+            puts "We are going to close the socket to Server"
+            -1
+         elsif data.length == 8 && data == "\x24\x45\x04\x00\x00\x00\x00\x00"
+            puts "\r\nGet End of stream msg \$E "
+            -1
+         else 
             data.length
-        }
-        #Send first request to get client_id
-        c.perform
-        @url = c.last_effective_url
-        puts "last effetive url is " + @url
-        if c.response_code != 200
-            print "HTTP return code is "+ c.response_code.to_s + " exit\r\n"
-            exit -1
-        else
-            print "HTTP return code is 200, Good\r\n"
-        end
-        content_len = '0'
-        content_len = c.header_str.scan(/Content-Length:\s+(\d+)/).flatten[0]
-        client_id = c.header_str.scan(/client-id=(\d+)/).flatten[0]
-        #Get client ID
-        puts "CLIENT ID is #{client_id} "
-        #puts "Content-Length is #{content_len}"
+         end
+      }
+      c.on_header {|data| 
+         print "HTTP Header: "+ data.chomp + " Size of header is: " + data.length.to_s + "\r\n"
+         if data =~ /Content-Length/
+            @post_log = false
+         end
+         data.length
+      }
 
-        c.headers = ["User-Agent: NSPlayer/11.0.5721.5251"]
-        c.headers += ["Pragma: no-cache,rate=1.000,stream-time=0,stream-offset=4294967295:4294967295,packet-num=4294967295,max-duration=0"]
-        if @speed == 1.0 
-            c.headers += ["Pragma: xPlayStrm=1","Pragma: LinkBW=2147483647\, AccelBW=2147483647\, AccelDuration=10000"]
-        else 
-            c.headers += ["Pragma: xPlayStrm=1","Pragma: LinkBW=2147483647\, AccelBW=2147483647\, AccelDuration=10000, Speed=#{@speed}"]
-        end
-        c.headers += ["Pragms: client-id=#{client_id}"]
-        c.headers += ["Supported: com.microsoft.wm.srvppair, com.microsoft.wm.sswitch, com.microsoft.wm.predstrm"]
-        c.headers += ["Pragma: xClientGUID={3300AD50-2C39-46c0-AE0A-4FAB6317C555}"]
-        c.headers += ["Pragma: stream-switch-count=2"]
-        c.headers += ["Pragma: stream-switch-entry=ffff:1:0 ffff:2:0"]
-        c.headers += ["Accept-Language: en-us, *;q=0.1"]
-        @total_packets = 0
-        c.on_body { |data| 
-            if @verbose
-                print "Get One body with length: "+ data.length.to_s + "\r\n"
-            end
-            print "#"
-            @total_bytes += data.length
-            @total_packets += 1
-            #End of Stream $E
-            if c.total_time > @duration
-                puts "We are going to close the socket to Server"
-                -1
-            elsif data.length == 8 && data == "\x24\x45\x04\x00\x00\x00\x00\x00"
-                puts "\r\nGet End of stream msg \$E "
-                -1
-            else 
-                data.length
-            end
-        }
-        c.on_header {|data| 
-            print "HTTP Header: "+ data.chomp + " Size of header is: " + data.length.to_s + "\r\n"
-            data.length
-        }
+      begin
+         c.http_get
+      rescue Curl::Err::WriteError => e
+         puts e.message
+      end
 
-        begin
-            c.http_get
-        rescue Curl::Err::WriteError => e
-            puts e.message
-        end
+      puts "Sumary : Total bytes  " + @total_bytes.to_s + " Total packets " + @total_packets.to_s
 
-        puts "Sumary : Total bytes  " + @total_bytes.to_s + " Total packets " + @total_packets.to_s
+   end
 
-    end
-
-    def post_log 
-        log_xml_data =  <<XML_STR
+   def post_log 
+      if @post_log == false
+         return 
+      end
+      log_xml_data =  <<XML_STR
     <XML>
     <Summary>0.0.0.0 2012-01-10 21:46:38 - http://wmt-os.auto.com:8800/pinball.wmv 0 2 5 200 {3300AD50-2C39-46c0-AE0A-8692EAFE4CA1} 11.0.5721.5275 en-US WMFSDK/11.0.5721.5275_WMPlayer/11.0.5721.5280 - wmplayer.exe 11.0.5721.5145 Windows_XP 5.1.0.2600 Pentium 22 836560 3054307 http TCP - - - - 836879 - 549 0 0 0 0 0 0 1 0 100 - - - - http://wmt-os.auto.com:8800/pinball.wmv pinball.wmv - </Summary>
     <c-ip>0.0.0.0</c-ip>
