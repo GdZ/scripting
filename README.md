4a9c1b12f2a5b3d75566643bd8a88ab0bcb1eb90
Herry Wang <tech.herry@gmail.com>
2012-12-11 14:18:49 -0800

    adding readme and command

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository is the container for common used scripts, including bash, js, nodejs, ruby, haskell, etc.
---
+++ herry.cmd
@@ -0,0 +1,6 @@
+git push cgit
+#origin is set to github, so after push, the commit will be seen in github.
+git push
+#cd ../cgit
+#run git pull to get latest
+#then the changes will be seen in the cgit website.
