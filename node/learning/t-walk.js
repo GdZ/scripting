5f82bbe3d8727aff3b5061dc8214df39f37fcfe4
Herry Wang <tech.herry@gmail.com>
2013-05-10 13:39:14 -0700

    move video downloader to other project

--- node/learning/t-walk.js
+++ node/learning/t-walk.js
@@ -1,16 +1,19 @@
 var walk = require('walk'); 
-var directoryPath = ".";
 
-var walker  = walk.walk(directoryPath, { followLinks: false });
+
+var directoryPath = "/home/herry/workspace/node-photoviewer/public/pics/";
+//var directoryPath = "./";
+
+//var walker  = walk.walk(directoryPath, { followLinks: false });
+var walker  = walk.walk(directoryPath, { followLinks: true });
 
 walker.on("directories", function (root, dirStatsArray, next) {
    // dirStatsArray is an array of `stat` objects with the additional attributes
    // * type
    // * error
    // * name
-   //console.log(root);
-   //console.log(dirStatsArray);
-
+   console.log(root);
+   console.log(dirStatsArray);
    next();
 });
 
@@ -28,13 +31,13 @@ walker.on('file', function(root, stat, next) {
       name: stat.name,
       root: root
    };
-//
-//{ 
-//    type: 'file',
-//    name: 'Buffer.js',
-//    root: './node_modules/exif/lib/exif'
-//},
-//
+   //
+   //{ 
+   //    type: 'file',
+   //    name: 'Buffer.js',
+   //    root: './node_modules/exif/lib/exif'
+   //},
+   //
    files.push(file);
    next();
 
@@ -43,5 +46,10 @@ walker.on('file', function(root, stat, next) {
 walker.on('end', function() {
    console.log(files);
 });
+
+walker.on('symlink', function(symlink, stat) {
+   //supported ? NO
+   console.log('Got symlink: ' + symlink)
+})
 
 
